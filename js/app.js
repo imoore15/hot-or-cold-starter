adc9218860ca3a851fa643d863fe7cb3b94e6ea4
imoore15 <imoore1789@gmail.com>
2016-05-12 23:02:39 -0400

    hccomplete

--- js/app.js
+++ js/app.js
@@ -1,8 +1,15 @@
-
+var rannum;
+var user;
 $(document).ready(function(){
+  
+  
+  newgame();
+  console.log(rannum);
 	
 	/*--- Display information modal box ---*/
-  	$(".what").click(function(){
+  	
+
+    $(".what").click(function(){
     	$(".overlay").fadeIn(1000);
 
   	});
@@ -12,6 +19,51 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+    $("#guessButton").click(function(event){
+      event.preventDefault();
+      user = $('#userGuess').val();
+      $('#guessList').append('<li>' + user + '</li>');
+      $('#count').text(parseInt($('#count').text()) +1);
+      
+      if (rannum == user) {
+        $('#feedback').text('CORRECT');
+
+      }
+
+      else if (Math.abs(rannum - user) > 50) {
+        $('#feedback').text('Ice Cold');
+      }
+
+      else if (Math.abs(rannum - user) > 25) {
+        $('#feedback').text('Warm');
+      }
+
+      else if (Math.abs(rannum - user) > 10) {
+        $('#feedback').text('Warmer');
+      }
+
+      else if (Math.abs(rannum - user) > 5) {
+        $('#feedback').text('On Fire');
+      }
+    
+    });
 });
 
 
+  	function newgame() {
+      rannum = Math.floor(Math.random()*101);
+      
+  	}
+
+
+
+    
+
+
+  
+
+    
+
+
+//* $("#myID").method(function() {}); *//
+//* function() {}; *//
