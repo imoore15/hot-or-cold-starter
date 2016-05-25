980e55727b15cc8636736f8862da2960ebbc9be1
imoore15 <imoore1789@gmail.com>
2016-05-24 23:08:06 -0400

    colorss

--- js/app.js
+++ js/app.js
@@ -5,6 +5,20 @@ $(document).ready(function(){
   
   newgame();
   console.log(rannum);
+
+  $(".what").click(function() {
+    $(".overlay").show();
+
+  });
+
+
+    $(".new").click(function() {
+    rannum = Math.floor(Math.random()*101);
+    console.log(rannum);
+    $('#feedback').text('Make your Guess!');
+    $('#count').text('0');
+    $('#guessList').empty();
+  });
 	
 	/*--- Display information modal box ---*/
   	
@@ -22,6 +36,7 @@ $(document).ready(function(){
     $("#guessButton").click(function(event){
       event.preventDefault();
       user = $('#userGuess').val();
+      $('#userGuess').val('');
       $('#guessList').append('<li>' + user + '</li>');
       $('#count').text(parseInt($('#count').text()) +1);
       
@@ -30,8 +45,24 @@ $(document).ready(function(){
 
       }
 
-      else if (Math.abs(rannum - user) > 50) {
+      else if (Math.abs(rannum - user) > 99) {
         $('#feedback').text('Ice Cold');
+      }
+
+      else if (Math.abs(rannum - user) > 75) {
+        $('#feedback').text('Cold');
+      }
+
+      else if (Math.abs(rannum - user) > 65) {
+        $('#feedback').text('Less Cold');
+      }
+
+      else if (Math.abs(rannum - user) > 50) {
+        $('#feedback').text('Coldish');
+      }
+
+      else if (Math.abs(rannum - user) > 35) {
+        $('#feedback').text('Warmish');
       }
 
       else if (Math.abs(rannum - user) > 25) {
@@ -45,6 +76,11 @@ $(document).ready(function(){
       else if (Math.abs(rannum - user) > 5) {
         $('#feedback').text('On Fire');
       }
+
+      else if (Math.abs(rannum - user) > 1) {
+        $('#feedback').text('Wow');
+      }
+
     
     });
 });
@@ -53,11 +89,7 @@ $(document).ready(function(){
   	function newgame() {
       rannum = Math.floor(Math.random()*101);
       
-  	}
-
-
-
-    
+  	};   
 
 
   
